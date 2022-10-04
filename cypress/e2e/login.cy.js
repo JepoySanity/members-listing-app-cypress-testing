84c27d2fc6e4a9a1fc2be7fbb94fcb410bc34e63
jepoy <factoran20@gmail.com>
2022-10-04 10:48:22 +0800

    remove initial click

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,9 +5,8 @@ describe('Loggin In - Basic Auth', () => {
 
   it('passes', () => {
     cy.visit('https://master.dhd7igftovw0h.amplifyapp.com/')
-    cy.get('button[type="submit"]').click()
     cy.get('input[name="username"]').type(username)
     cy.get('input[name="password"]').type(password)
-    // cy.get('button[type="submit"]').click()
+    cy.get('button[type="submit"]').click()
   })
 })
